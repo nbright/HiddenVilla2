45cd569627f60769719f60a3a87df489f788ef1d
nbright <kms3700@gmail.com>
2021-12-14 06:53:11 +0900

    a

--- DataAccess/Migrations/20211213204859_AddYRRoomToDb2.Designer.cs
+++ DataAccess/Migrations/20211213204859_AddYRRoomToDb2.Designer.cs
@@ -4,14 +4,16 @@ using DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211213204859_AddYRRoomToDb2")]
+    partial class AddYRRoomToDb2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
